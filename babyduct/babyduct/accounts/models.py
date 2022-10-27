5ff876db7600c7fcd513f40e852923f269e0ad77
odulaja philip <odulajaphilip@gmail.com>
2022-10-27 19:34:38 +0100

    created Customer Profile model

--- babyduct/babyduct/accounts/models.py
+++ babyduct/babyduct/accounts/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer_Profile(models.Model):
+    user = models.OneToOneField(User)
+    gender = models.CharField()
+    age = models.IntegerField()
+    contact_no = models.CharField()
+
+    
