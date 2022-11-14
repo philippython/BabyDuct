f85099c9240de2ac45e73fff18828d3f51c368e9
odulaja philip <odulajaphilip@gmail.com>
2022-11-14 20:28:31 +0100

    created producers app

--- babyduct/accounts/models.py
+++ babyduct/accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer_Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image  =  models.ImageField()
     gender = models.CharField(max_length=6)
     age = models.IntegerField()
     contact_no = models.CharField(max_length=11)
