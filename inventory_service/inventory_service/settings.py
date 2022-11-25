f439eeedd8651cba7bfbe569cc70db9e1be00b92
odulaja philip <odulajaphilip@gmail.com>
2022-11-25 16:36:11 +0100

    update inventory secret key

--- inventory_service/inventory_service/settings.py
+++ inventory_service/inventory_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z!0c@602e@q!cpf2x)_!^)d+m*u=%f$n%tu49f1*24a9q8)paa'
-
+SECRET_KEY = os.getenv('INVENTORY_SERVICE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
