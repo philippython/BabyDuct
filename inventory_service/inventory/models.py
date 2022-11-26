149f9b7481682bc4823800ed29d2e2f029311cc5
odulaja philip <odulajaphilip@gmail.com>
2022-11-26 21:32:57 +0100

    created Product and review model

--- inventory_service/inventory/models.py
+++ inventory_service/inventory/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    image = models.ImageField()
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField(max=5)
+    review = models.CharField(max_length=255)
