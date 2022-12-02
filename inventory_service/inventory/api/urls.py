3538efc30944b19e9d4eee1c28449642dd14e618
odulaja philip <odulajaphilip@gmail.com>
2022-12-02 13:41:02 +0100

    modified inventory urls module

--- inventory_service/inventory/api/urls.py
+++ inventory_service/inventory/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductCreateView, ReviewCreateView
+from .views import ProductCreateView, ReviewCreateView, ProductListView
 
 urlpatterns = [
+    path("all-products", ProductListView.as_view(), name="all_products")
     path("new-product", ProductCreateView.as_view(), name="create_product"),
     path("new-review", ReviewCreateView.as_view(), name="review_product"),
 ]
