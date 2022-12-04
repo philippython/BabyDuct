bdfb7b0406667786dfd297187f505a6bb01640fc
odulaja philip <odulajaphilip@gmail.com>
2022-12-04 19:00:31 +0100

    updated inventory view

--- inventory_service/inventory/api/views.py
+++ inventory_service/inventory/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, CreateAPIView
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from inventory.models import Product
 from inventory.api.serializers import ProductSerializers, ReviewSerializers, ProductsImageSerializers
@@ -9,14 +10,16 @@ class ProductCreateView(CreateAPIView):
     serializer_class = ProductSerializers
 
 class ProductListView(ListCreateAPIView):
+    serializer_class = ProductSerializers
     queryset = Product.objects.all()
 
 class ProductDetailView(RetrieveAPIView):
     serializer_class = ProductSerializers
 
-    def get(self, request):
-        product = Product.objects.get(slug=request.slug)
-        return product
+    def get(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        serializer = ProductSerializers(product)
+        return Response(serializer.data, 200)
 
 class ReviewCreateView(CreateAPIView):
     serializer_class = ReviewSerializers
