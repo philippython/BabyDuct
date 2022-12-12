c548edece1fe503649aa1c3e71860278a6abb9cb
odulaja philip <odulajaphilip@gmail.com>
2022-12-12 21:08:40 +0100

    created static schema

--- inventory_service/inventory_service/urls.py
+++ inventory_service/inventory_service/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/inventory/', include('inventory.api.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/v1/inventory/', include('inventory.api.urls')),
+    path('openapi', get_schema_view(title="BabyDuct Inventory Service API", 
+                                    description="The Inventory API that handles every product information",
+                                    version="1.0.0"),
+                                    name='openapi-schema')] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
