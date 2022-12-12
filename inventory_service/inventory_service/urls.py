96add616cc63bfbb067ea2b0a820a03df125d9c7
odulaja philip <odulajaphilip@gmail.com>
2022-12-12 21:15:41 +0100

    rename swagger ui template file

--- inventory_service/inventory_service/urls.py
+++ inventory_service/inventory_service/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/inventory/', include('inventory.api.urls')),
+    path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html',extra_context={'schema_url':'openapi-schema'}), name='swagger-ui'),
     path('openapi', get_schema_view(title="BabyDuct Inventory Service API", 
                                     description="The Inventory API that handles every product information",
                                     version="1.0.0"),
