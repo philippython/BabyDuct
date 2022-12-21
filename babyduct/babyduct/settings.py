a5c455a180240f9119c060c595c24488d76eb76f
odulaja philip <odulajaphilip@gmail.com>
2022-12-21 21:55:56 +0100

    modified babyduct settings

--- babyduct/babyduct/settings.py
+++ babyduct/babyduct/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
