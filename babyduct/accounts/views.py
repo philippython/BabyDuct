c516e838ad93073448ae8edbac327878e803a6af
odulaja philip <odulajaphilip@gmail.com>
2022-12-23 21:28:27 +0100

    updated account service views

--- babyduct/accounts/views.py
+++ babyduct/accounts/views.py
@@ -37,3 +37,36 @@ class ConsumerProfileDeleteView(DestroyAPIView):
     def perform_destroy(self, instance):
         user = get_object_or_404(User, username=instance.user)
         user.delete()
+
+
+
+class ConsumerProfileCreateView(CreateAPIView):
+    serializer_class = ConsumerProfileSerializers
+
+
+class ConsumerListAPIView(ListAPIView):
+    serializer_class = ConsumerProfileSerializers
+    queryset = ConsumerProfile.objects.all()
+
+
+class ConsumerProfileView(RetrieveAPIView):
+    serializer_class = ConsumerProfileSerializers
+
+    def get(self, request, uuid):
+        consumer = get_object_or_404(ConsumerProfile, user=uuid)
+        serializer = ConsumerProfileSerializers(consumer)
+        return Response(serializer.data, 200)
+
+class ConsumerProfileUpdateView(UpdateAPIView):
+    serializer_class = ConsumerProfileSerializers
+    queryset = ConsumerProfile.objects.all()
+    lookup_field = "user"
+
+class ConsumerProfileDeleteView(DestroyAPIView):
+    serializer_class = ConsumerProfileSerializers
+    queryset = ConsumerProfile.objects.all()
+    lookup_field = "user"
+
+    def perform_destroy(self, instance):
+        user = get_object_or_404(User, username=instance.user)
+        user.delete()
