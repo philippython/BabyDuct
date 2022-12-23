d95140bc9991c666b09f8d03791a869508e5ed49
odulaja philip <odulajaphilip@gmail.com>
2022-12-23 21:33:39 +0100

    testing account service views

--- babyduct/accounts/urls.py
+++ babyduct/accounts/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     path('consumer/all', ConsumerListAPIView.as_view(), name='all_consumers'),
     path('consumer/profile/<str:uuid>', ConsumerProfileView.as_view(), name='consumer_pofile'),
     path('consumer/profile/<str:user>/edit', ConsumerProfileUpdateView.as_view(), name='edit_consumer_pofile'),
-    path('consumer/profile/<str:user>/delete', ConsumerProfileDeleteView.as_view(), name='delete_consumer_profile')
+    path('consumer/profile/<str:user>/delete', ConsumerProfileDeleteView.as_view(), name='delete_consumer_profile'),
 
+
+    # producer urls
+    path('producer/create-profile', ProducerProfileCreateView.as_view(), name='create_producer_profile'),
+    path('producer/all', ProducerListAPIView.as_view(), name='all_producers'),
+    path('producer/profile/<str:uuid>', ProducerProfileView.as_view(), name='producer_pofile'),
+    path('producer/profile/<str:user>/edit', ProducerProfileUpdateView.as_view(), name='edit_producer_pofile'),
+    path('producer/profile/<str:user>/delete', ProducerProfileDeleteView.as_view(), name='delete_producer_profile')
 ]
--- babyduct/accounts/views.py
+++ babyduct/accounts/views.py
@@ -39,32 +39,32 @@ class ConsumerProfileDeleteView(DestroyAPIView):
         user.delete()
 
 
-
-class ConsumerProfileCreateView(CreateAPIView):
-    serializer_class = ConsumerProfileSerializers
-
-
-class ConsumerListAPIView(ListAPIView):
-    serializer_class = ConsumerProfileSerializers
-    queryset = ConsumerProfile.objects.all()
+# producer views here
+class ProducerProfileCreateView(CreateAPIView):
+    serializer_class = ProducerProfileSerializers
 
 
-class ConsumerProfileView(RetrieveAPIView):
-    serializer_class = ConsumerProfileSerializers
+class ProducerListAPIView(ListAPIView):
+    serializer_class = ProducerProfileSerializers
+    queryset = ProducerProfile.objects.all()
+
+
+class ProducerProfileView(RetrieveAPIView):
+    serializer_class = ProducerProfileSerializers
 
     def get(self, request, uuid):
-        consumer = get_object_or_404(ConsumerProfile, user=uuid)
-        serializer = ConsumerProfileSerializers(consumer)
+        Producer = get_object_or_404(ProducerProfile, user=uuid)
+        serializer = ProducerProfileSerializers(Producer)
         return Response(serializer.data, 200)
 
-class ConsumerProfileUpdateView(UpdateAPIView):
-    serializer_class = ConsumerProfileSerializers
-    queryset = ConsumerProfile.objects.all()
+class ProducerProfileUpdateView(UpdateAPIView):
+    serializer_class = ProducerProfileSerializers
+    queryset = ProducerProfile.objects.all()
     lookup_field = "user"
 
-class ConsumerProfileDeleteView(DestroyAPIView):
-    serializer_class = ConsumerProfileSerializers
-    queryset = ConsumerProfile.objects.all()
+class ProducerProfileDeleteView(DestroyAPIView):
+    serializer_class = ProducerProfileSerializers
+    queryset = ProducerProfile.objects.all()
     lookup_field = "user"
 
     def perform_destroy(self, instance):
