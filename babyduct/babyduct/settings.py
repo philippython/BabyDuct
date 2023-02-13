b5049a5d302476cbdfed4cfab07135ddaf33cad2
odulaja philip <odulajaphilip@gmail.com>
2023-02-13 06:52:14 +0100

    Deployed: modified settings.py

--- babyduct/babyduct/settings.py
+++ babyduct/babyduct/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('ACCOUNT_SERVICE_SECRET_KEY')
+SECRET_KEY = "django-insecure-6o@vr%8^-kz%ei=*^9r04bd)f*)f=c85_iz5f4y+0a9&w@1!z#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
