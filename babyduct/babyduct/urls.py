086da9205cb39366ac8ed5decc84faae201759ea
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-02-21 22:57:48 +0100

    Update urls.py

--- babyduct/babyduct/urls.py
+++ babyduct/babyduct/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 from dj_rest_auth.views import PasswordResetConfirmView
@@ -48,4 +47,6 @@ urlpatterns = [
         description="API for BabyDuct Users",
         version="1.0.0"
     ), name='accounts-schema'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
