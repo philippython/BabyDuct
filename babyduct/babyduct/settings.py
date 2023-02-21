e37eb627a3839c75aa257d3fec1dad1d210e9276
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-02-21 10:44:00 +0100

    Update settings.py

--- babyduct/babyduct/settings.py
+++ babyduct/babyduct/settings.py
@@ -168,9 +168,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_RENDERER_CLASSES': [
-    #     'rest_framework.renderers.JSONRenderer',
-    # ],
+#     'DEFAULT_RENDERER_CLASSES': [
+#         'rest_framework.renderers.JSONRenderer',
+#     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
