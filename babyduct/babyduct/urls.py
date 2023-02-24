b8801bcf07e51fee8a00740d0151581151fa55d9
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-02-25 00:40:04 +0100

    Update urls.py

--- babyduct/babyduct/urls.py
+++ babyduct/babyduct/urls.py
@@ -1,4 +1,4 @@
-"""babyduct URL Configuration
+""" babyduct URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
