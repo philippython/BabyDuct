fd81870732009c8f51473c8a1f209aef7a7d5fd5
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-03-26 22:59:38 +0100

    updated git ignore file

---
+++ babyduct/accounts/api/helper.py
@@ -0,0 +1,24 @@
+import string
+KEY = 5
+
+alpha = string.ascii_lowercase + "0123456789/ "
+
+def char_encrypt(key, string):
+    cipher = ""
+    for letter in string:
+        if letter in string:
+            new_position = ( alpha.index(letter) + key ) % len(alpha)
+            cipher += alpha[new_position]
+        else :
+            cipher += letter
+    return cipher
+
+def char_decrypt(key, string):
+    cipher = ""
+    for letter in string:
+        if letter in string:
+            new_position = ( alpha.index(letter) - key ) % len(alpha)
+            cipher += alpha[new_position]
+        else :
+            cipher += letter
+    return cipher
