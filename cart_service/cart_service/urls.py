08abaf53b3aaa0480d9bba9a78dd1cf22e48c35e
chidera6 <dictasteil@gmail.com>
2023-04-17 14:59:15 +0100

    fix:added whitenoise to requirements file

--- cart_service/cart_service/urls.py
+++ cart_service/cart_service/urls.py
@@ -4,7 +4,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Cart Service API",
@@ -25,3 +24,4 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
  
 ]
+
