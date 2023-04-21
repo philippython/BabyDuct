40beea68794f25dc12421a76c8c7525315384b03
chidera6 <dictasteil@gmail.com>
2023-04-21 18:49:57 +0100

    fix:modified the settings file

--- cart_service/cart_service/settings.py
+++ cart_service/cart_service/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
 """  
 #POstgres production daabase
 DATABASES   = {
-       'default':dj_database_url.parse(env('DATABASE_URL'))
+       'default':dj_database_url.parse('postgres://production_o3f6_user:OfPwNIfFpTRrxHy48tlLmJaLOrI0Xam7@dpg-cgs7mh9jvhtj5p6qa190-a.ohio-postgres.render.com/production_o3f6')
 }
 
 # Password validation
