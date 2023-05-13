9db776844e2714f40940d692929e86f2c6d97c6c
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-05-13 06:45:02 +0100

    Bug fix: static files configuration

--- inventory_service/inventory_service/urls.py
+++ inventory_service/inventory_service/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, staticfiles_urlpatterns
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 
 schema_view = get_schema_view(openapi.Info(
       title="Babyduct accounts service API Documentation",
@@ -43,4 +41,4 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
