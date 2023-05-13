614a66dd9e6e9f41aa951943e5af3ca79394f0fa
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-05-13 05:58:41 +0100

    Bug fix: Media file configuration

--- babyduct/babyduct/urls.py
+++ babyduct/babyduct/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from accounts.api.views import FacebookLogin, TwitterLogin, GoogleLogin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from accounts.api.views import FacebookLogin, TwitterLogin, GoogleLogin
 
 schema_view = get_schema_view(openapi.Info(
       title="Babyduct accounts service API Documentation",
@@ -43,6 +44,4 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/accounts/', include('accounts.api.urls')),
-]
-
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
